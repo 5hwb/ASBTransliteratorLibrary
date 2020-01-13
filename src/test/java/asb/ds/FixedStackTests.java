db7c30b81bc1e5ab4a8f71f2430ac166c4d953d9
Perry Hartono <perry@desktopPC>
2020-01-13 14:29:50 +1100

    Update FixedStack unit tests

--- src/test/java/asb/ds/FixedStackTests.java
+++ src/test/java/asb/ds/FixedStackTests.java
@@ -17,9 +17,35 @@ public class FixedStackTests {
 	}
 	
 	@Test
+	public void test_fill() {
+		fixedStack.fill(8);
+		assertEquals(8, fixedStack.top().intValue());
+		assertEquals(8, fixedStack.nthTop(1).intValue());
+		assertEquals(8, fixedStack.nthTop(2).intValue());
+	}
+	
+	@Test
 	public void test() {
 		fixedStack.push(2);
 		assertEquals(2, fixedStack.top().intValue());
+		assertNull(fixedStack.nthTop(1));
+		assertNull(fixedStack.nthTop(2));
+		assertNull(fixedStack.nthTop(3));
+
+		fixedStack.push(7);
+		assertEquals(7, fixedStack.top().intValue());
+		assertEquals(2, fixedStack.nthTop(1).intValue());
+		assertNull(fixedStack.nthTop(2));
+
+		fixedStack.push(3);
+		assertEquals(3, fixedStack.top().intValue());
+		assertEquals(7, fixedStack.nthTop(1).intValue());
+		assertEquals(2, fixedStack.nthTop(2).intValue());
+
+		fixedStack.push(38);
+		assertEquals(38, fixedStack.top().intValue());
+		assertEquals(3, fixedStack.nthTop(1).intValue());
+		assertEquals(7, fixedStack.nthTop(2).intValue());
 	}
 
 }
